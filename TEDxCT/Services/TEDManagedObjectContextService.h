75a7e01869c3b9f5cf9d430ced5241febba1ba1f
Daniel Galasko <danielhgalasko@gmail.com>
2014-04-02 19:01:49 +0200

    added a launch view controller with a random image, moved core data setup out of app delegate. will create a core data manager soon

---
+++ TEDxCT/Services/TEDManagedObjectContextService.h
@@ -0,0 +1,13 @@
+//
+//  TEDManagedObjectContextService.h
+//  TEDxCT
+//
+//  Created by Daniel Galasko on 3/31/14.
+//  Copyright (c) 2014 TEDxCapeTown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TEDManagedObjectContextService : NSObject
+
+@end
---
+++ TEDxCT/View Controllers/TEDLaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEDLaunchViewController.h
+//  TEDxCT
+//
+//  Created by Daniel Galasko on 3/31/14.
+//  Copyright (c) 2014 TEDxCapeTown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEDLaunchViewController : UIViewController
+
+@end
