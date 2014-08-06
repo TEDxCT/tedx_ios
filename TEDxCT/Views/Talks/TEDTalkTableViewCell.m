4cfc5bd4046543a79d2722ccf83f4db0cb069b03
Daniel Galasko <danielhgalasko@gmail.com>
2014-08-06 20:24:26 +0200

    The Talk Cells are now opaque

--- TEDxCT/Views/Talks/TEDTalkTableViewCell.m
+++ TEDxCT/Views/Talks/TEDTalkTableViewCell.m
@@ -14,7 +14,10 @@
 {
     self.talkImageView.layer.cornerRadius = CGRectGetWidth(self.talkImageView.frame)/2;
     self.talkImageView.layer.masksToBounds = YES;
-
+    UIColor *backgroundColor = self.contentView.backgroundColor;
+    self.talkNameLabel.backgroundColor = backgroundColor;
+    self.talkSpeakerName.backgroundColor = backgroundColor;;
+    self.genre.backgroundColor = backgroundColor;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
